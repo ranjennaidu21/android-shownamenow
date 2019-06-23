232f95132e2076902fb17f61876ebb73ec50564a
ranjen <ranjennaidu21@gmail.com>
2019-06-23 17:06:11 +0800

    First App Introduction - Show Entered Name when click button

--- app/src/main/java/com/ranjen/shownamenow/MainActivity.java
+++ app/src/main/java/com/ranjen/shownamenow/MainActivity.java
@@ -3,7 +3,9 @@ package com.ranjen.shownamenow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     //create new button and textview object as in the view
     private Button myButton;
     private TextView myTextView;
+    private EditText myEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +22,18 @@ public class MainActivity extends AppCompatActivity {
 
         //refer the object to the button
         myButton = findViewById(R.id.button1);
+        myEditText = findViewById(R.id.editText1);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //show the entered text in input once clicked the button
+                String input1 = myEditText.getText().toString();
+
+                //text to displayed when clicked on the button1
+                myTextView.setText("Hello " + input1);
+            }
+        });
         myTextView = findViewById(R.id.textView1);
 
-        //test to overwrite the UI text
-        myTextView.setText("Text 2");
     }
 }
